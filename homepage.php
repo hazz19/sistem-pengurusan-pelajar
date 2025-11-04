a0873afa768a9f7036f59780891bf7ec08ff60fe
Irfezn <irfanbhl07@gmail.com>
2025-11-04 12:05:34 +0800

    dtng balik

--- homepage.php
+++ homepage.php
@@ -13,6 +13,8 @@
     <div class="content">
         <h1>Selamat Datang Ke Sistem Maklumat Pelajar KVDSAZI</h1>
         <button class="login-btn"><a href="lgpg.php">LOGIN</a></button>
+        <p>Belum Mempunyai Akaun? Daftar Sekarang</p>
+        <button class="register-btn"><a href="daftar.php">DAFTAR AKAUN</a></button>
     </div>
 </body>
 </html>
